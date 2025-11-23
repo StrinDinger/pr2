db567569cfda6ab25fe1e8585226be1df5c06629
grigorys <samyshkin.grigory@mail.ru>
2025-11-23 15:24:15 +0300

    zoom in fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import { LanguageProvider } from '../contexts/LanguageContext';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      </head>
       <body>
         <LanguageProvider>
           {children}
